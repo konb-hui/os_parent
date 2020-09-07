402eec741f1adcbcce410320b9fa3a20142032db
konb-hui <2581881495@qq.com>
2020-09-07 22:11:00 +0800

    Construction of project environment

---
+++ service/service_edu/src/main/java/com/konb/eduservice/mapper/EduTeacherMapper.java
@@ -0,0 +1,16 @@
+package com.konb.eduservice.mapper;
+
+import com.konb.eduservice.entity.EduTeacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-09-06
+ */
+public interface EduTeacherMapper extends BaseMapper<EduTeacher> {
+
+}
---
+++ service/service_edu/src/main/java/com/konb/eduservice/service/impl/EduTeacherServiceImpl.java
@@ -0,0 +1,20 @@
+package com.konb.eduservice.service.impl;
+
+import com.konb.eduservice.entity.EduTeacher;
+import com.konb.eduservice.mapper.EduTeacherMapper;
+import com.konb.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-09-06
+ */
+@Service
+public class EduTeacherServiceImpl extends ServiceImpl<EduTeacherMapper, EduTeacher> implements EduTeacherService {
+
+}
