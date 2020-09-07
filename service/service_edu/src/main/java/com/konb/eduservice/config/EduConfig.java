3a580390ef02059b454d59d1056c622bd2391824
konb-hui <2581881495@qq.com>
2020-09-08 00:11:51 +0800

    Integrate Swagger

--- service/service_edu/src/main/java/com/konb/eduservice/config/EduConfig.java
+++ service/service_edu/src/main/java/com/konb/eduservice/config/EduConfig.java
@@ -5,9 +5,11 @@ import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @MapperScan("com.konb.eduservice.mapper")
+@EnableSwagger2
 public class EduConfig {
 
     /**
