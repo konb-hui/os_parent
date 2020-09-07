e22649692e9679f3215ca1369b983d15db785b4c
konb-hui <2581881495@qq.com>
2020-09-07 23:09:46 +0800

    Query and delete function of background instructor module

--- service/service_edu/src/main/java/com/konb/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/konb/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,12 @@
 package com.konb.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.konb.eduservice.entity.EduTeacher;
+import com.konb.eduservice.service.EduTeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +19,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/eduservice/edu-teacher")
 public class EduTeacherController {
+
+    //把service注入
+    @Autowired
+    private EduTeacherService teacherService;
+
+    //查询讲师表所有数据
+    //rest风格
+    @GetMapping("findAll")
+    public List<EduTeacher> findAllTeacher() {
+        //调用service的方法实心调用
+        List<EduTeacher> teachers = teacherService.list(null);
+        return teachers;
+    }
+
+    //逻辑删除讲师的方法
+    @DeleteMapping("{id}")                                  //表示id需要路径来传递
+    public boolean removeTeacher(@PathVariable String id) { //获取路径中的id值
+        return teacherService.removeById(id);
+    }
 
 }
 
