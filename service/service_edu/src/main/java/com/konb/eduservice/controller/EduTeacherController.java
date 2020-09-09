83e9a8b9e41ad17a9036727a61e3bc185a4f9889
konb-hui <2581881495@qq.com>
2020-09-09 23:08:00 +0800

    Unify the returned results by create a class named R

--- service/service_edu/src/main/java/com/konb/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/konb/eduservice/controller/EduTeacherController.java
@@ -1,8 +1,12 @@
 package com.konb.eduservice.controller;
 
 
+import com.konb.commonutils.R;
 import com.konb.eduservice.entity.EduTeacher;
 import com.konb.eduservice.service.EduTeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +20,7 @@ import java.util.List;
  * @author testjava
  * @since 2020-09-06
  */
+@Api(description = "讲师管理")
 @RestController
 @RequestMapping("/eduservice/edu-teacher")
 public class EduTeacherController {
@@ -27,17 +32,25 @@ public class EduTeacherController {
     //查询讲师表所有数据
     //rest风格
     @GetMapping("findAll")
-    public List<EduTeacher> findAllTeacher() {
+    @ApiOperation(value = "获得所有讲师")
+    public R findAllTeacher() {
         //调用service的方法实心调用
         List<EduTeacher> teachers = teacherService.list(null);
-        return teachers;
+        return R.ok().data("items", teachers);
     }
 
     //逻辑删除讲师的方法
     @DeleteMapping("{id}")                                  //表示id需要路径来传递
-    public boolean removeTeacher(@PathVariable String id) { //获取路径中的id值
-        return teacherService.removeById(id);
+    @ApiOperation(value = "逻辑删除讲师")
+    public R removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true) @PathVariable String id) { //获取路径中的id值
+        boolean flag = teacherService.removeById(id);
+        if (flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
     }
+
 
 }
 
